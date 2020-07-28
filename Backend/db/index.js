95ed96d6797da92732371e9c7d3e1191cce13952
Aluko Ayodele <51384137+Alukoayodele@users.noreply.github.com>
2020-07-28 02:09:38 +0100

    more updates

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-require('dotenv').config();
+
 const conString =
   'postgres://xkyjplyb:8JY4KHGtExYaEEga_4dkVwJugdRBaKEF@ruby.db.elephantsql.com:5432/xkyjplyb';
 const client = new Client(conString);
