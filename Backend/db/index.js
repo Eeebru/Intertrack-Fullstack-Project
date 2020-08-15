09ebeb79672ab53ab6c4aedaae27ea896b986ee5
unknown <alaoopeyemi101@gmail.com>
2020-08-15 15:23:51 +0100

    Add same order check before buying a new order

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,24 +1,7 @@
-// const { Client } = require('pg');
-// require('dotenv').config();
-// const conString = process.env.CON_STRING;
-// const client = new Client(conString);
-// client.connect();
-// module.exports = client;
-const { Client } = require('pg');
+const { Client } = require('pg');
+require('dotenv').config();
+const CON_STRING = process.env.conString;
+const client = new Client(CON_STRING);
+client.connect();
 
-const conString =
-  'postgres://xkyjplyb:8JY4KHGtExYaEEga_4dkVwJugdRBaKEF@ruby.db.elephantsql.com:5432/xkyjplyb';
-const client = new Client(conString);
-client.connect(function (err) {
-  if (err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT NOW() AS "theTime"', function (err, result) {
-    if (err) {
-      return console.error('error running query', err);
-    }
-    console.log('Contected to Database');
-    client.end();
-  });
-});
-module.exports = client;
+module.exports = client;
--- Backend/routes/order.js
+++ Backend/routes/order.js
@@ -30,15 +30,21 @@ router.post('/orderproduct', auth, async (req, res) => {
 
     //these are pre-set
     const user_id = req.id,
-      order_date = new Date(),
-      expiry_date = new Date(new Date().setMonth(new Date().getMonth() + 1)), //add one month to the order date
-      is_active = expiry_date > order_date ? true : false; //this would always be true, we would use this "(expiry_date > expiry_date ? false : true );" to update it from the front, or we might just make do with order and expiry date to determing if the order is active or not from the front.
-
+    order_date = new Date(),
+    expiry_date = new Date(new Date().setMonth(new Date().getMonth() + 1)), //add one month to the order date
+    is_active = expiry_date > order_date ? true : false; //this would always be true, we would use this "(expiry_date > expiry_date ? false : true );" to update it from the front, or we might just make do with order and expiry date to determing if the order is active or not from the front.
+    
+    const uniqueOrder = await db.query(`SELECT * FROM public."order" WHERE user_id = $1`, [req.id]);
+    const userOrders = uniqueOrder.rows;
+    userOrders.map(order => {
+      if (order.product_id === product_id && order.expiry_date > new Date() ) {
+        res.status(401).json({message: 'You already have this plan'})
+      }
+    })
     const order = await db.query(
-      `INSERT INTO public."order" (user_id, product_id, order_date, expiry_date, is_active) VALUES ($1,$2,$3,$4,$5) RETURNING * `,
-      [user_id, product_id, order_date, expiry_date, is_active]
+    `INSERT INTO public."order" (user_id, product_id, order_date, expiry_date, is_active) VALUES ($1,$2,$3,$4,$5) RETURNING * `,
+    [user_id, product_id, order_date, expiry_date, is_active]
     );
-
     res.status(200).json({ result: order.rows[0] });
   } catch (error) {
     console.log(error);
