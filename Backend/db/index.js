1922e1e9209f9c4ddae388d23dbbc2510b0fbf10
Aluko Ayodele <51384137+Alukoayodele@users.noreply.github.com>
2020-08-15 13:12:51 +0100

    minor edit

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,6 +1,24 @@
+// const { Client } = require('pg');
+// require('dotenv').config();
+// const conString = process.env.CON_STRING;
+// const client = new Client(conString);
+// client.connect();
+// module.exports = client;
 const { Client } = require('pg');
-require('dotenv').config();
-const conString = process.env.CON_STRING;
+
+const conString =
+  'postgres://xkyjplyb:8JY4KHGtExYaEEga_4dkVwJugdRBaKEF@ruby.db.elephantsql.com:5432/xkyjplyb';
 const client = new Client(conString);
-client.connect();
+client.connect(function (err) {
+  if (err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function (err, result) {
+    if (err) {
+      return console.error('error running query', err);
+    }
+    console.log('Contected to Database');
+    client.end();
+  });
+});
 module.exports = client;
